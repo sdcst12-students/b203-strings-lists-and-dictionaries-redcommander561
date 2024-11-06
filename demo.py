c384852207f3114bdf63bb2b15a1a6fbc4cb9a17
redcommander561 <jaysona803@deltalearns.ca>
2024-11-06 11:45:25 -0800

    .
    .

---
+++ demo.py
@@ -0,0 +1,7 @@
+inv = {
+    'food' : 2,
+    'water' : 3
+}
+
+inv['food'] = 7
+print( inv['food'] )
--- task5.py
+++ task5.py
@@ -1,15 +1,41 @@
 items = {
-
-
-
-
+    "weapon": "bow",
+    "defense":  "shield",
+    "armour":  "chainmail",
+    "food": "magic apple"
 }
+
+itemsSorted = []
 inventory = []
 while True:
     print("----------------------------------------")
-    print("you can either choose item, get item, or see inventory.")
+    print("you can choose to quit, drop items, get item, or see inventory.")
     print("----------------------------------------")
+    A = input("input: ")
+    if A == "quit":
+        exit()
+    if A == 'get item':  
+        print(items)
+        get = input("choose an item from their class: ")
+        if get == "bow" or "shield" or "chainmail" or "magic apple":
+            inventory.append(get)
+            print(inventory)
+        
+    if A == "drop item":
+        print(inventory)
+        print("this is your inventory")
+        drop = input("choose which item you would like to drop: ")
+        if drop == "bow" or drop == "shield" or drop=="chainmail" or drop=="magic apple":
+            a = inventory.index(drop)
+            #find the index/position of the item you are trying to drop
+            inventory.pop(a)
+            print(inventory)
+        elif items in inventory == 0:
+            print("you have nothing to drop")
+    if A == "see inventory":
+        print(inventory)
+        print("this is your inventory! you can add or drop items into your inventory")
 
-    A = input(" ")
-    if A == 'choose item':  
     
+
+       
